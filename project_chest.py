9980c2343c203230961d0c68aa13804ec6229405
Rebbekkah <90262374+Rebbekkah@users.noreply.github.com>
2021-10-01 16:03:19 +0200

    Add files via upload

--- project_chest.py
+++ project_chest.py
@@ -1,13 +1,60 @@
-import matplotlib.pyplot as plt
+'''import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
 import glob
 import tensorflow as tf
+import seaborn as sns
 from tensorflow import keras
 from tensorflow.keras import layers
 from sklearn import preprocessing
 from matplotlib import pyplot
+'''
+
+import pandas as pd       
+import matplotlib as mat
+import matplotlib.pyplot as plt    
+import numpy as np
+import seaborn as sns
+# %matplotlib inline
+
+pd.options.display.max_colwidth = 100
+
+import random
+import os
+
+from numpy.random import seed
+seed(42)
+
+random.seed(42)
+os.environ['PYTHONHASHSEED'] = str(42)
+os.environ['TF_DETERMINISTIC_OPS'] = '1'
+
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import callbacks
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import glob
+import cv2
+
+from tensorflow.random import set_seed
+set_seed(42)
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+IMG_SIZE = 224
+BATCH = 32
+SEED = 42
+
 
 # main_path = 
 main_path = "/home/sdv/m2bi/rgoulancourt/AI/Kaggle/data/archive/chest_xray/chest_xray"
@@ -39,4 +86,119 @@ test_list.extend([x for x in test_pneumonia])
 df_test = pd.DataFrame(np.concatenate([['Normal']*len(test_normal) , ['Pneumonia']*len(test_pneumonia)]), columns = ['class'])
 df_test['image'] = [x for x in test_list]
 
-print(df_train)
+print(df_train, df_test)
+
+# Preparing the data
+train_df, val_df = train_test_split(df_train, test_size = 0.20, random_state = SEED, stratify = df_train['class'])
+
+print(train_df, val_df)
+
+
+# Image loading from folders
+
+train_datagen = ImageDataGenerator(rescale=1/255.,
+                                  zoom_range = 0.1,
+                                  #rotation_range = 0.1,
+                                  width_shift_range = 0.1,
+                                  height_shift_range = 0.1)
+
+val_datagen = ImageDataGenerator(rescale=1/255.)
+
+ds_train = train_datagen.flow_from_dataframe(train_df,
+                                             #directory=train_path, #dataframe contains the full paths
+                                             x_col = 'image',
+                                             y_col = 'class',
+                                             target_size = (IMG_SIZE, IMG_SIZE),
+                                             class_mode = 'binary',
+                                             batch_size = BATCH,
+                                             seed = SEED)
+
+ds_val = val_datagen.flow_from_dataframe(val_df,
+                                            #directory=train_path,
+                                            x_col = 'image',
+                                            y_col = 'class',
+                                            target_size = (IMG_SIZE, IMG_SIZE),
+                                            class_mode = 'binary',
+                                            batch_size = BATCH,
+                                            seed = SEED)
+
+ds_test = val_datagen.flow_from_dataframe(df_test,
+                                            #directory=test_path,
+                                            x_col = 'image',
+                                            y_col = 'class',
+                                            target_size = (IMG_SIZE, IMG_SIZE),
+                                            class_mode = 'binary',
+                                            batch_size = 1,
+                                            shuffle = False)
+
+
+
+# CNN
+
+#Setting callbakcs
+
+early_stopping = callbacks.EarlyStopping(
+    monitor='val_loss',
+    patience=5,
+    min_delta=0.0000001,
+    restore_best_weights=True,
+)
+
+plateau = callbacks.ReduceLROnPlateau(
+    monitor='val_loss',
+    factor = 0.2,                                     
+    patience = 2,                                   
+    min_delt = 0.0000001,                                
+    cooldown = 0,                               
+    verbose = 1
+) 
+
+def get_model():
+    
+    #Input shape = [width, height, color channels]
+    inputs = layers.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
+    
+    # Block One
+    x = layers.Conv2D(filters=16, kernel_size=3, padding='valid')(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.MaxPool2D()(x)
+    x = layers.Dropout(0.2)(x)
+
+    # Block Two
+    x = layers.Conv2D(filters=32, kernel_size=3, padding='valid')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.MaxPool2D()(x)
+    x = layers.Dropout(0.2)(x)
+    
+    # Block Three
+    x = layers.Conv2D(filters=64, kernel_size=3, padding='valid')(x)
+    x = layers.Conv2D(filters=64, kernel_size=3, padding='valid')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.MaxPool2D()(x)
+    x = layers.Dropout(0.4)(x)
+
+    # Head
+    #x = layers.BatchNormalization()(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Dropout(0.5)(x)
+    
+    #Final Layer (Output)
+    output = layers.Dense(1, activation='sigmoid')(x)
+    
+    model = keras.Model(inputs=[inputs], outputs=output)
+    
+    return model
+
+
+
+keras.backend.clear_session()
+
+model = get_model()
+model.compile(loss='binary_crossentropy'
+              , optimizer = keras.optimizers.Adam(learning_rate=0.00003), metrics='binary_accuracy')
+
+model.summary()
